b1c3977ea0282aa8051a7401bdf7fe071b99ae85
tysonthompson <56133772+tysonthompson@users.noreply.github.com>
2020-11-05 23:23:58 -0500

    Completed Electric Cars and Information Activities

--- src/main/java/com/example/saveourearth/Tutorial.java
+++ src/main/java/com/example/saveourearth/Tutorial.java
@@ -2,28 +2,13 @@ package com.example.saveourearth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.WindowManager;
-import android.widget.MediaController;
-import android.widget.VideoView;
 
 public class Tutorial extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_tutorial);
-
-        VideoView videoView = findViewById(R.id.vV);
-        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.tutorial;
-        Uri uri = Uri.parse(videoPath);
-        videoView.setVideoURI(uri);
-
-        MediaController mediaController = new MediaController(this);
-        videoView.setMediaController(mediaController);
-        mediaController.setAnchorView(videoView);
-
     }
 }
